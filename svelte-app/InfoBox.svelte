406245e3895bc43081793d64e776cc16e48f6b18
Kaenigsion <herklotzpaul@gmail.com>
2023-11-03 17:14:27 +0100

    fixed typos

--- svelte-app/InfoBox.svelte
+++ svelte-app/InfoBox.svelte
@@ -9,11 +9,11 @@
             Sie können die Bausteine von rechts in die Felder ziehen und auf ein
             Feld drücken, um es zu einer Ausgabe-LED zu machen.
             <h3>2. Simulieren</h3>
-            Wenn Sie nun auf den "Run"-button klicken beziehungsweise die Enter-Taste
+            Wenn Sie nun auf den "Run"-button klicken, beziehungsweise die Enter-Taste
             drücken, gelangen Sie in den Simulationsmodus. Sie können mit dem Klicken
-            auf die nun erschienden oberen Eingabe-LED's das Programm simulieren
+            auf die nun erschienenen oberen Eingabe-LED's das Programm simulieren
             und sehen, wie sich die anderen LED's ändern. Wenn Sie nun eine Änderung
-            vornehmen werden Sie zurück zum Bau-Modus gebracht.
+            vornehmen, werden Sie zurück zum Bau-Modus gebracht.
         </div>
     </div>
 </div>
