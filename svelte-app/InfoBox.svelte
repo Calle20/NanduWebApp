430a09051320566bddb09f4b69db46c65c49c1d0
Kaenigsion <herklotzpaul@gmail.com>
2023-11-12 16:47:35 +0100

    updated help box

--- svelte-app/InfoBox.svelte
+++ svelte-app/InfoBox.svelte
@@ -7,13 +7,16 @@
         <div class="card card-body" style="width: 350px">
             <h3>1. Erstellen</h3>
             Sie können die Bausteine von rechts in die Felder ziehen und auf ein
-            Feld drücken, um es zu einer Ausgabe-LED zu machen.
+            Feld drücken, um es zu einer Ausgabe-LED zu machen, wo nun ein 'L' steht.
+            Mit erneutem Drücken können sie diese LED zu einer Eingabe-LED konvertieren,
+            welche mit 'Q' gekennzeichnet sind.
             <h3>2. Simulieren</h3>
             Wenn Sie nun auf den "Run"-button klicken, beziehungsweise die Enter-Taste
             drücken, gelangen Sie in den Simulationsmodus. Sie können mit dem Klicken
-            auf die nun erschienenen oberen Eingabe-LED's das Programm simulieren
-            und sehen, wie sich die anderen LED's ändern. Wenn Sie nun eine Änderung
-            vornehmen, werden Sie zurück zum Bau-Modus gebracht.
+            auf die nun erschienenen oberen und die selbst kreirtern Eingabe-LEDs
+            das Programm simulieren und sehen, wie sich die anderen LEDs ändern.
+            Wenn Sie nun eine Änderung vornehmen, werden Sie zurück zum Bau-Modus
+            gebracht.
         </div>
     </div>
 </div>
